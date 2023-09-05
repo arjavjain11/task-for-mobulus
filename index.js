2951372e39261cfa2343bc3a4199669fb20c776c
Arjav jain <jainarjav11@gmail.com>
2023-09-05 11:29:59 +0530

    Some changes

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import cors from 'cors';
 import 'dotenv/config'
-import cronJobs from './cron/cron.js';
 import routers from "./routes/index.js";
-
-cronJobs();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -15,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use('/', routers);
 
-mongoose.connect('mongodb://localhost:27017/news_feeds', {
+mongoose.connect('mongodb://localhost:27017/news_feed', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
